f9f6d3baf0a63860a417596b4e95174644dea777
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-26 15:26:34 +0530

    update price

--- src/store/items/actions.js
+++ src/store/items/actions.js
@@ -1,8 +1,30 @@
 export const ITEM_ADDED = 'ITEM_ADDED';
+export const ITEM_REMOVED = 'ITEM_REMOVED';
+export const ITEM_PRICE_UPDATED = 'ITEM_PRICE_UPDATED';
 
 export const addNewItem = (name, price) => ({
-    type: ITEM_ADDED,
-    payload: {
-        name, price
-    }
+  type: ITEM_ADDED,
+  payload: {
+    name,
+    price
+  }
 });
+
+export const removeItem = (uuid) => {
+  return {
+    type: ITEM_REMOVED,
+    payload: {
+      uuid
+    }
+  };
+};
+
+export const updatePrice = (uuid, price) => {
+  return {
+    type: ITEM_PRICE_UPDATED,
+    payload: {
+      uuid,
+      price
+    }
+  };
+};
