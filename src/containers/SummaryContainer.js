3982c161c47a606a0e9f30aa2d148c5498826c5e
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-27 17:25:03 +0530

    use reselect as selectors

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -1,17 +1,26 @@
 import { connect } from 'react-redux';
 import { Summary } from '../components/Summary';
+import {
+  selectSubtotal,
+  selectTipAmount,
+  selectTotal
+} from '../store/items/selectors';
 
 const mapStateToProps = (state) => {
-  const items = state.items;
+  // const items = state.items;
 
-  const subtotal = items.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0
-  );
+  // const subtotal = items.reduce(
+  //   (sum, item) => sum + item.price * item.quantity,
+  //   0
+  // );
 
-  const tipAmount = subtotal * (state.tipPercentage / 100);
+  // const tipAmount = subtotal * (state.tipPercentage / 100);
 
-  const total = subtotal + tipAmount;
+  // const total = subtotal + tipAmount;
+
+  const subtotal = selectSubtotal(state);
+  const tipAmount = selectTipAmount(state);
+  const total = selectTotal(state);
 
   return {
     subtotal,
