aeec160e9de7073e52f8bd03565adacfa14bb754
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-27 17:52:17 +0530

    use immer for reducer

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   ITEM_ADDED,
   ITEM_PRICE_UPDATED,
@@ -12,32 +13,70 @@ export const initialItems = [
   { uuid: id++, name: 'Awesome Vegan Ham', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+// export const reducer = (state = initialItems, action) => {
+//   // if (action.type === ITEM_ADDED) {
+//   //   const item = { uuid: id++, quantity: 1, ...action.payload };
+//   //   return [...state, item];
+//   // }
+//   if (action.type === ITEM_ADDED) {
+//     return produce(state, (draftState) => {
+//       const item = { uuid: id++, quantity: 1, ...action.payload };
+//       draftState.push(item);
+//     });
+//   }
+//   if (action.type === ITEM_REMOVED) {
+//     return state.filter((item) => item.uuid !== action.payload.uuid);
+//   }
+//   if (action.type === ITEM_PRICE_UPDATED) {
+//     return produce(state, (draftState) => {
+//       const item = draftState.find((item) => item.uuid === action.payload.uuid);
+//       item.price = parseInt(action.payload.price, 10);
+//     });
+//   }
+//   if (action.type === ITEM_QUANTIITY_UPDATED) {
+//     return produce(state, (draftState) => {
+//       const item = draftState.find((item) => item.uuid === action.payload.uuid);
+//       item.quantity = parseInt(action.payload.quantity, 10);
+//     });
+//   }
+//   // if (action.type === ITEM_PRICE_UPDATED) {
+//   //   return state.map((item) => {
+//   //     if (item.uuid === action.payload.uuid) {
+//   //       return { ...item, price: action.payload.price };
+//   //     }
+//   //     return item;
+//   //   });
+//   // }
+//   // if (action.type === ITEM_QUANTIITY_UPDATED) {
+//   //   return state.map((item) => {
+//   //     if (item.uuid === action.payload.uuid) {
+//   //       return { ...item, quantity: action.payload.quantity };
+//   //     }
+//   //     return item;
+//   //   });
+//   // }
+
+//   return state;
+// };
+
+// ======================using immer==========================
+export const reducer = produce((state=initialItems, action) => {
   if (action.type === ITEM_ADDED) {
-    const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+      const item = { uuid: id++, quantity: 1, ...action.payload };
+      state.push(item);
   }
   if (action.type === ITEM_REMOVED) {
     return state.filter((item) => item.uuid !== action.payload.uuid);
   }
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
-    });
+      const item = state.find((item) => item.uuid === action.payload.uuid);
+      item.price = parseInt(action.payload.price, 10);
   }
   if (action.type === ITEM_QUANTIITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
-    });
+      const item = state.find((item) => item.uuid === action.payload.uuid);
+      item.quantity = parseInt(action.payload.quantity, 10);
   }
+}, initialItems);
 
-  return state;
-};
 
 export default reducer;
